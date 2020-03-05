f2e39435d9ab6e393a6292aed203258c32ebda6b
KiKiKi <chaika.design@gmail.com>
2020-03-06 04:10:01 +0900

    Don't need transform setting by babel-jest. Because Jest included babel-jest and if has babel config in project, automatically transform by it.

--- jest.config.js
+++ jest.config.js
@@ -158,9 +158,9 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  transform: {
-    "^.+.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
-  },
+  //transform: {
+  //  "^.+.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+  //},
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
